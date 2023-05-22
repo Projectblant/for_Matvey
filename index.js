fb5198fbece6c0785e16ecdd7d196fc4d182e9b7
skipperfuck <114660113+Kiyotaka-aynokodzi@users.noreply.github.com>
2023-05-22 17:49:52 +0500

    u

---
+++ index.js
@@ -0,0 +1,20 @@
+function sss() {
+
+let toogle = document.querySelector('#toggle')
+let p = document.querySelector('#p')
+
+toogle.addEventListener('mouseup', () => {
+    p.innerHTML = 'Offline'
+    p.style.color = 'red'
+    toogle.style.backgroundColor = 'red'
+})
+
+toogle.addEventListener('mousedown', () => {
+    p.innerHTML = 'online'
+    p.style.color = 'green'
+    toogle.style.backgroundColor = 'green'
+})
+}
+
+
+
